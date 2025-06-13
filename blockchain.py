199242a131f40d1dd9dcdc66b8b209b2d4d3458c
raw465 <rawane.cerbouche7@gmail.com>
2025-06-13 22:11:24 +0200

    Added blockchain structure with Block and Blockchain classes

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,48 @@
+import hashlib
+import json
+from time import time
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.compute_hash()
+
+    def compute_hash(self):
+        block_string = json.dumps({
+            'index': self.index,
+            'timestamp': self.timestamp,
+            'data': self.data,
+            'previous_hash': self.previous_hash
+        }, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, time(), {"message": "Genesis Block"}, "0")
+        self.chain.append(genesis_block)
+
+    def get_last_block(self):
+        return self.chain[-1]
+
+    def add_block(self, data):
+        last_block = self.get_last_block()
+        new_block = Block(len(self.chain), time(), data, last_block.hash)
+        self.chain.append(new_block)
+
+    def is_valid(self):
+        for i in range(1, len(self.chain)):
+            current = self.chain[i]
+            previous = self.chain[i - 1]
+            if current.hash != current.compute_hash():
+                return False
+            if current.previous_hash != previous.hash:
+                return False
+        return True
 
